41d9605d2086d8e4295633bcce27cd98011fc32c
BilieuR <bilieureath1@gmail>
2017-02-07 18:15:52 +1000

    Added imageshack and paperclip, updated pins views

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,5 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
